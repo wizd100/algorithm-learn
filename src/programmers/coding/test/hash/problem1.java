f76fa912b96e8b076a531b8e412a0527bd9864c3
bkh-mac <wizd100@gmail.com>
2019-09-05 18:21:55 +0900

    프로그래머스 코딩 테스트 해시 완주하지 못한 선수 문제 필요없는 코드들 제거 테스트는 통과하지만 효율성에서 통과하질 못함

--- src/programmers/coding/test/hash/problem1.java
+++ src/programmers/coding/test/hash/problem1.java
@@ -3,27 +3,17 @@ package programmers.coding.test.hash;
 public class problem1 {
     public static void main(String[] args) {
         problem1 sol = new problem1();
-        String[] participant = {"leo", "kiki", "eden"};
-        String[] completion = {"eden", "kiki"};
-        //String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        //String[] completion = {"stanko", "ana", "mislav"};
+        //String[] participant = {"leo", "kiki", "eden"};
+        //String[] completion = {"eden", "kiki"};
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
         sol.solution1(participant, completion);
     }
 
-    //중복이름이 있을경우 해결이 안됨
-    //n * n -1 최대수행
     public String solution1(String[] participant, String[] completion) {
         for(int i = 0; i < participant.length; i++) {
-            int count = 0;
             for(int j = 0; j < completion.length; j++) {
-                if(!participant[i].equals(completion[j])) {
-                    count++;
-                    if(count == completion.length) {
-                        System.out.println("완주하지 못한 선수 : " + participant[i]);
-                        return participant[i];
-                    }
-                } else {
-                    // 참가자와 완주자 중복된 이름을 해결하기 위해 같다면 참가자의 이름을 지움
+                if(participant[i].equals(completion[j])) {
                     participant[i] = "";
                     completion[j] = "";
                 }
