4cd3d0b16eb27d5d9e93386041892fbde31a41b9
bkh-mac <wizd100@gmail.com>
2020-02-25 17:43:19 +0900

    codeup 1096번 문제 자바 소스코드 좀더 쉽고 짧게 수정

--- src/codeup/p1096.java
+++ src/codeup/p1096.java
@@ -5,35 +5,18 @@ import java.util.Scanner;
 public class p1096 {
     public static void main(String[] args) {
         int inputNum;
-        int[] x;
-        int[] y;
-        int[][] go = new int[19][19];
+        int[][] map = new int[20][20];
 
         Scanner scanner = new Scanner(System.in);
         inputNum = scanner.nextInt();
-        x = new int[inputNum];
-        y = new int[inputNum];
 
         for (int i = 0; i < inputNum; i++) {
-            x[i] = scanner.nextInt() - 1;
-            y[i] = scanner.nextInt() - 1;
+            map[scanner.nextInt()][scanner.nextInt()] = 1;
         }
 
-        for (int i = 0; i < go.length; i++) {
-            for (int j = 0; j < go[i].length; j++) {
-                boolean flag = false;
-
-                for (int k = 0; k < inputNum; k++) {
-                    if (i == x[k] && j == y[k]) {
-                        flag = true;
-                    }
-                }
-
-                if (flag) {
-                    System.out.print("1 ");
-                } else {
-                    System.out.print("0 ");
-                }
+        for (int i = 1; i < map.length; i++) {
+            for (int j = 1; j < map[i].length; j++) {
+                System.out.printf("%d ", map[i][j]);
             }
             System.out.print("\n");
         }
