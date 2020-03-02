a9474c814041d4a1a2b260fe29642fc9e537ea38
bkh-mac <wizd100@gmail.com>
2020-03-02 18:08:00 +0900

    codeup 1901 재귀함수 자바 소스코드 깔끔하게 변경

--- src/codeup/p1901.java
+++ src/codeup/p1901.java
@@ -4,21 +4,17 @@ import java.util.Scanner;
 
 public class p1901 {
     public static void main(String[] args) {
-        int count = 1;
-        int inputNum;
-
         Scanner scanner = new Scanner(System.in);
-        inputNum = scanner.nextInt();
 
-        recursion(count, inputNum);
+        recursion(1, scanner.nextInt());
     }
 
-    public static void recursion(int count, int inputNum) {
-        if (count > inputNum) {
+    public static void recursion(int count, int maxNum) {
+        if (count > maxNum) {
             return;
-        } else {
-            System.out.println(count);
-            recursion(count + 1, inputNum);
         }
+
+        System.out.println(count);
+        recursion(count + 1, maxNum);
     }
 }
