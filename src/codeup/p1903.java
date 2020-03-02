4fd7e81cf1934ab317791e829565774646fe6f17
bkh-mac <wizd100@gmail.com>
2020-03-02 18:47:06 +0900

    codeup 1903 자바 소스코드

---
+++ src/codeup/p1903.java
@@ -0,0 +1,22 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class p1903 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        recursion(scanner.nextInt(), scanner.nextInt());
+    }
+
+    public static void recursion(int a, int b) {
+        if (a > b) {
+            return;
+        }
+        if (a % 2 != 0) {
+            System.out.printf("%d ", a);
+        }
+
+        recursion(a + 1, b);
+    }
+}
