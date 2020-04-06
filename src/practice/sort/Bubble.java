fcc3edf96f388c01c6bd540575350dfdb91d075f
bkh-mac <wizd100@gmail.com>
2020-04-06 17:11:43 +0900

    결과값 주석으로 넣음

--- src/practice/sort/Bubble.java
+++ src/practice/sort/Bubble.java
@@ -18,6 +18,7 @@ public class Bubble {
             }
         }
 
+        //1 4 5 8 10 13 19 21 25 30
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("%d ", arr[i]);
         }
