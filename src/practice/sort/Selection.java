c60a95290337b16cd04fa413602652e1354e9d46
bkh-mac <wizd100@gmail.com>
2020-04-08 16:59:01 +0900

    선택 정렬 깔끔하게 수정

--- src/practice/sort/Selection.java
+++ src/practice/sort/Selection.java
@@ -3,23 +3,21 @@ package practice.sort;
 public class Selection {
     public static void main(String[] args) {
         int[] arr = {10, 5, 4, 1, 14, 19, 3, 13, 2, 18};
+        int minIndex;
+        int temp;
 
         for (int i = 0; i < arr.length; i++) {
-            int min = arr[i];
-            int minIndex = i;
-            int temp;
+            minIndex = i;
 
-            for (int j = 0 + i; j < arr.length - 1; j++) {
-                if (min > arr[j + 1]) {
-                    min = arr[j + 1];
-                    minIndex = j + 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
-
             }
 
-            temp = arr[0 + i];
-            arr[0 + i] = arr[minIndex];
-            arr[minIndex] = temp;
+            temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
 
         //1 2 3 4 5 10 13 14 18 19
