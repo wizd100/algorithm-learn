11e8825c501daf0cad423a2460e67cb4b6af1148
bkh-mac <wizd100@gmail.com>
2020-07-28 15:30:21 +0900

    이진탐색 좀더 코드를 깔끔하게 정리

--- src/doit/chap03/Q4.java
+++ src/doit/chap03/Q4.java
@@ -6,21 +6,19 @@ public class Q4 {
     static int binarySearch(int[] a, int n, int key) {
         int start = 0;
         int end = n - 1;
-        int mid = (start + end) / 2;
 
-        while (true) {
-            if (a[mid] == key){
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (a[mid] == key) {
                 return mid;
             } else if (a[mid] > key) {
                 end = mid - 1;
-                mid = (start + end) / 2;
-            } else if (a[mid] < key) {
+            } else {
                 start = mid + 1;
-                mid = (start + end) / 2;
-            } else if (start <= end){
-                return -1;
             }
         }
+
+        return -1;
     }
 
     public static void main(String[] args) {
