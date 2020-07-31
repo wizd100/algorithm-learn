c1ec20e355b3e8bcdde722bf7befcd3d56f40fca
bkh-mac <wizd100@gmail.com>
2020-07-31 23:34:18 +0900

    백준 9012 스택 문제 일부분만 올림

---
+++ src/baekjoon/P9012.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class P9012 {
+    //vps -> 스택의 총 수는 짝수가 되여야 함
+    //맨 마지막 값은 무조건 )
+    //( , ) 값이 똑같이 있어야함 = 총 수의 절반
+    //맨뒤 ) 값과 그 앞부터 ( 값을 탐색한 다음 지움 그리고 앞으로 민다
+    //)( -> 닫히지 않았음으로 NO
+    //
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        scanner.nextLine();
+
+        String s;
+        String[] arr;
+        for (int i = 0; i < T; i++) {
+            s = scanner.nextLine();
+            arr = s.split("");
+
+            //짝수가 아니면 vps가 아님
+            if (arr.length % 2 != 0) {
+                System.out.println("NO");
+                break;
+            }
+            //맨 마지막 값이 )이 아니면 vps가 아님
+            if (!arr[arr.length - 1].equals(")")) {
+                System.out.println("NO )");
+                break;
+            }
+            
+        }
+    }
+}
