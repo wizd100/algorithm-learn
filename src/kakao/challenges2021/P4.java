2408ad2b91d5bb194094d64f6f866422fc6f1de1
bkh <wizd100@gmail.com>
2020-09-12 13:15:56 +0900

    카카오 2021 테스트 미리 파일을 만듬

---
+++ src/kakao/challenges2021/P4.java
@@ -0,0 +1,7 @@
+package kakao.challenges2021;
+
+public class P4 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/kakao/challenges2021/P5.java
@@ -0,0 +1,7 @@
+package kakao.challenges2021;
+
+public class P5 {
+    public static void main(String[] args) {
+
+    }
+}
