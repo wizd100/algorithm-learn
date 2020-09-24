b7f81d571d9e59c2aacc43b144809fa1d83a444f
bkh-mac <wizd100@gmail.com>
2020-09-24 17:52:10 +0900

    백준 10250 필요없는 코드 제거

--- src/baekjoon/P10250.java
+++ src/baekjoon/P10250.java
@@ -16,27 +16,7 @@ public class P10250 {
             N[i] = sc.nextInt();
         }
 
-
         for (int i = 0; i < T; i++) {
-            int[][] hotel = new int[H[i]][W[i]];
-
-            //H층 j
-            for (int j = H[i]; j > 0; j--) {
-                //W호 k
-                for (int k = 1; k <= W[i]; k++) {
-                    hotel[H[i] - j][k - 1] = Integer.parseInt(String.format("%d%02d", j, k));
-                    //System.out.printf("%d%02d ", j, k);
-                }
-                //System.out.println();
-            }
-
-            /*for (int j = 0; j < hotel.length; j++) {
-                for (int k = 0; k < hotel[j].length; k++) {
-                    System.out.printf("%d ", hotel[j][k]);
-                }
-                System.out.println();
-            }*/
-
             //N 번째 방번호 찾기
             //호를 고정으로 하고 층만 올라감
             int count = 1;
@@ -44,16 +24,12 @@ public class P10250 {
             for (int k = 1; k <= W[i]; k++) {
                 for (int j = 1; j <= H[i]; j++) {
                     if (N[i] == count) {
-                        //System.out.printf("R : %d%02d ", j, k);
                         System.out.printf("%d%02d\n", j, k);
                         break forout;
                     }
-                    //System.out.printf("%d%02d ", j, k);
                     count++;
                 }
-                //System.out.println();
             }
         }
-
     }
 }
