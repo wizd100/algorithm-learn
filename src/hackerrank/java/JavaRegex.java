17863550878397c7cd702de40e7c9fd36be446eb
bkh-mac <wizd100@gmail.com>
2020-10-13 18:47:23 +0900

    업데이트

---
+++ src/hackerrank/java/JavaRegex.java
@@ -0,0 +1,18 @@
+package hackerrank.java;
+
+import java.util.Scanner;
+
+public class JavaRegex {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    public String pattern = "\\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\\b";
+}
---
+++ src/practice/test/Arr.java
@@ -0,0 +1,13 @@
+package practice.test;
+
+public class Arr {
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        int[] arr2;
+        arr2 = new int[3];
+
+        int[] arr3 = {1, 2, 3};
+
+
+    }
+}
