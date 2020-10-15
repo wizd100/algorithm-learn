afddfee825858ebbb0c36d412148b4270c2ba4f4
bkh-mac <wizd100@gmail.com>
2020-10-15 18:05:11 +0900

    백준 9020 통과는 하는데 4초이상 걸림

--- src/baekjoon/P9020.java
+++ src/baekjoon/P9020.java
@@ -1,5 +1,6 @@
 package baekjoon;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class P9020 {
@@ -7,14 +8,14 @@ public class P9020 {
         Scanner sc = new Scanner(System.in);
         int caseMax = 10000;
         boolean[] number = new boolean[caseMax + 1];
+        ArrayList<Integer> prime = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
 
-        number[0] = true;
-        number[1] = true;
-        for (int i = 2; i * i <= caseMax; i++) {
+        for (int i = 2; i <= caseMax; i++) {
             if (number[i]) {
                 continue;
             }
+            prime.add(i);
             for (int j = i + i; j <= caseMax; j += i) {
                 number[j] = true;
             }
@@ -25,7 +26,7 @@ public class P9020 {
         for (int i = 0; i < T; i++) {
             int n = sc.nextInt();
 
-            int[] result = partition(number, n);
+            int[] result = partition(prime, n);
 
             sb.append(result[0] + " " + result[1]);
             sb.append("\n");
@@ -34,24 +35,19 @@ public class P9020 {
         System.out.println(sb);
     }
 
-    public static int[] partition(boolean[] number, int n) {
+    public static int[] partition(ArrayList<Integer> prime, int n) {
         int min = n;
         int[] result = new int[2];
 
-        for (int i = 2; i <= n; i++) {
-            if (number[i]) {
-                continue;
-            }
-            for (int j = i; j <= n; j++) {
-                if (number[j]) {
+        for (int i = 0; i < prime.size(); i++) {
+            for (int j = i; j < prime.size(); j++) {
+                if (prime.get(i) + prime.get(j) != n) {
                     continue;
                 }
-                if (i + j == n) {
-                    if (min > j - i) {
-                        min = j - i;
-                        result[0] = i;
-                        result[1] = j;
-                    }
+                if (min > prime.get(j) - prime.get(i)) {
+                    min = prime.get(j) - prime.get(i);
+                    result[0] = prime.get(i);
+                    result[1] = prime.get(j);
                 }
             }
         }
